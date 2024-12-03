d883d27d8a28d96cd5156de24e25e6bd99737afc
SamuelSantos1997 <samuel.personal2021@gmail.com>
2024-12-03 09:43:44 -0300

    mudanças no controller

--- src/main/java/org/senac/Aula01/controller/ProdutoController.java
+++ src/main/java/org/senac/Aula01/controller/ProdutoController.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.data.domain.Sort;
 
 @RestController
 @RequestMapping("/produto")
@@ -23,10 +24,20 @@ public class ProdutoController {
     private ProdutoRepository repository;
 
     @GetMapping
-    public List<Produto> get(){
-        
-        return repository.findAll();
-    }
+    public List<Produto> get(@RequestParam(required = false, defaultValue = "preco") String order) {
+      Sort sort;
+  
+      // Verifica se o campo é "nome" ou "preco" e define a direção de ordenação
+      if (order.equals("preco")) {
+          // Se for "preco", ordena por DESC
+          sort = Sort.by(Sort.Direction.DESC, "preco");
+      } else {
+          // Se for "nome" ou qualquer outro valor, ordena por ASC
+          sort = Sort.by(Sort.Direction.ASC, "nome");
+      }
+  
+      return repository.findAll(sort);
+  }
 
     @PostMapping
      public Produto save(@RequestBody Produto produto){
