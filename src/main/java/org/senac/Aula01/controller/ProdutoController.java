4946353570091537d6a85c6f85deeff52c7c1523
SamuelSantos1997 <samuel.personal2021@gmail.com>
2024-12-03 19:56:17 -0300

    ajuste para filtrar

--- src/main/java/org/senac/Aula01/controller/ProdutoController.java
+++ src/main/java/org/senac/Aula01/controller/ProdutoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @RestController
 @RequestMapping("/produto")
@@ -24,20 +25,32 @@ public class ProdutoController {
     private ProdutoRepository repository;
 
     @GetMapping
-    public List<Produto> get(@RequestParam(required = false, defaultValue = "preco") String order) {
-      Sort sort;
-  
-      // Verifica se o campo é "nome" ou "preco" e define a direção de ordenação
-      if (order.equals("preco")) {
-          // Se for "preco", ordena por DESC
-          sort = Sort.by(Sort.Direction.DESC, "preco");
-      } else {
-          // Se for "nome" ou qualquer outro valor, ordena por ASC
-          sort = Sort.by(Sort.Direction.ASC, "nome");
+    public List<Produto> get(@RequestParam(required = false, defaultValue = "nome") String order, @RequestParam(required = false) String filter) {
+      Sort sort;    
+      // Verifica qual campo foi solicitado para ordenação
+      switch (order) {
+          case "maiorPreco":
+              // Ordena pelo campo "preco" em ordem decrescente
+              sort = Sort.by(Sort.Direction.DESC, "preco");
+              break;
+          case "menorPreco":
+              // Ordena pelo campo "preco" em ordem crescente
+              sort = Sort.by(Sort.Direction.ASC, "preco");
+              break;
+          case "nome":
+          default:
+              // Ordena pelo campo "nome" em ordem crescente
+              sort = Sort.by(Sort.Direction.ASC, "nome");
+              break;
       }
   
+      if (filter != null && !filter.isEmpty()) {
+        return repository.findByNomeContains(filter, sort);
+    }
+
       return repository.findAll(sort);
   }
+  
 
     @PostMapping
      public Produto save(@RequestBody Produto produto){
--- src/main/java/org/senac/Aula01/repository/ProdutoRepository.java
+++ src/main/java/org/senac/Aula01/repository/ProdutoRepository.java
@@ -1,8 +1,12 @@
 package org.senac.Aula01.repository;
 
+import java.util.List;
+
 import org.senac.Aula01.model.Produto;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
- 
+ List<Produto> findByNomeContains(String filter, Sort sort);
 }
+
